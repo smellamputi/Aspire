190abe7a51660f91349325a8c578de1d603b4e00
Divyanshu.Anand@docusign.com <Divyanshu.Anand@docusign.com>
2021-10-01 00:11:43 +0530

    ASP5422_AR_OB_Yaypay - Updated the iar files, DB and Intarchive,env files for issue ASP-8321

---
+++ ASP/Interface/OTC/Code/ASP5422_AR_OB_Yaypay/DB/Contact_SRC.sql
@@ -0,0 +1,20 @@
+CREATE TABLE CREATE_CONTACT_SRC_TBL(
+CODE_IDENTIFIER               VARCHAR2(5000), 
+SEQ                           VARCHAR2(5000), 
+BATCH_ID                      VARCHAR2(5000), 
+CUSTOMER_INTERNAL_ID          VARCHAR2(5000), 
+EMAIL                         VARCHAR2(5000), 
+FIRST_NAME                    VARCHAR2(5000), 
+LAST_NAME                     VARCHAR2(5000), 
+MOBILE_PHONE                  VARCHAR2(5000), 
+PHONE                         VARCHAR2(5000), 
+PRIMARY                       VARCHAR2(5000), 
+TITLE                         VARCHAR2(5000), 
+ADDITIONAL_EMAIL              VARCHAR2(5000), 
+RESPONSIBILITY_TYPE           VARCHAR2(5000), 
+JOB_TITLE_CODE                VARCHAR2(5000), 
+INTEGRATION_INTERNAL_ID       VARCHAR2(5000), 
+CUSTOMER_ACCOUNT_NUMBER       VARCHAR2(5000), 
+CUSTOMER_NAME                 VARCHAR2(5000), 
+RUN_DATE                      DATE           
+);
